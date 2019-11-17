d6b8381fcb514d91e40c7cc520f9667d3f2d163a
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2019-11-17 14:19:27 +0300

    build

---
+++ docs/precache-manifest.750ddb55fe937a565728397cc97387ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d39401a23e376e9f7cc7f76c776718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b066cc40df757cd28e69",
+    "url": "/static/css/main.cbd91e49.chunk.css"
+  },
+  {
+    "revision": "380015a3da27bd790cec",
+    "url": "/static/js/2.e0fbe25d.chunk.js"
+  },
+  {
+    "revision": "b066cc40df757cd28e69",
+    "url": "/static/js/main.d9650d76.chunk.js"
+  },
+  {
+    "revision": "8486c63dc1bde95f3649",
+    "url": "/static/js/runtime-main.aa308bd9.js"
+  }
+]);
