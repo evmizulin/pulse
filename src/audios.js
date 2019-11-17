07d033c49759d5641bd231ace90b09e2ac5ead48
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-11-18 00:37:58 +0300

    Add another djembe set to instruments

--- src/audios.js
+++ src/audios.js
@@ -4,6 +4,11 @@ export const audios = {
     new Audio('mp3/t1.mp3'),
     new Audio('mp3/s1.mp3'),
   ],
+  'djembe2': [
+    new Audio('mp3/b2.mp3'),
+    new Audio('mp3/t2.mp3'),
+    new Audio('mp3/s2.mp3'),
+  ],
   'djembe3': [
     new Audio('mp3/b3.mp3'),
     new Audio('mp3/t3.mp3'),
