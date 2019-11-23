d18ce6a953c5bec1f929ebf6ccd17579567e39db
GitHub Action <action@github.com>
2019-11-23 20:21:37 +0000

    Add changes

---
+++ docs/precache-manifest.64e67ce8919afc8244c6a52cc31d567e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07592afb455a3dfdfa1b7107be87505c",
+    "url": "/pulse/index.html"
+  },
+  {
+    "revision": "93c2185a6dd3ced6b72c",
+    "url": "/pulse/static/css/main.54f1c646.chunk.css"
+  },
+  {
+    "revision": "166af57ca85593a394d9",
+    "url": "/pulse/static/js/2.ffd07500.chunk.js"
+  },
+  {
+    "revision": "93c2185a6dd3ced6b72c",
+    "url": "/pulse/static/js/main.9d11ab29.chunk.js"
+  },
+  {
+    "revision": "e7335242eddb9b6f0017",
+    "url": "/pulse/static/js/runtime-main.7e1c53c5.js"
+  }
+]);
